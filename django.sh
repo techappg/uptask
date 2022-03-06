627430b1a7a5499e1e354e288598965eec18fe18
techappg <43813485+techappg@users.noreply.github.com>
2022-03-06 11:48:43 +0530

    Update django.sh
    update

--- django.sh
+++ django.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-cd /home/uptask
-
-git pull
+if [ -d "env" ]
+then
+    echo "Python virtual environment exists."
+else
+    echo "Creating Python virtual environment."
+    virtualenv -p python3.8 env
+fi
 
 source env/bin/activate
 
