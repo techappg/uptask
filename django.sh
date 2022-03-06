127034f91115ade9c36dab1f3144b0da27d87805
techappg <43813485+techappg@users.noreply.github.com>
2022-03-06 11:40:13 +0530

    Update django.sh
    update

--- django.sh
+++ django.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+cd /home/uptask
+
+git pull
+
+source env/bin/activate
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+
+python manage.py makemigration merge --noinput
+
+python manage.py migrate
+
+python manage.py test
